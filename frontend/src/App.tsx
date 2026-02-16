18baf9d17f828d39b0fa191cd696decbe1525530
YUG3011 <yug30112005@gmail.com>
2026-02-16 14:47:20 +0530

    change in zip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ interface ConversationItem {
   messages?: ChatMessage[];
 }
 
-const apiHome = 'http://localhost:3000/api';
+const apiHome = 'https://ai-support-rg-b0epdvbgcrb9c9ad.centralindia-01.azurewebsites.net/api';
 
 function App() {
   const [chatList, setChatList] = useState<ConversationItem[]>([]);
